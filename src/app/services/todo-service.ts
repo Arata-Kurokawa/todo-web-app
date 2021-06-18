a4bb381d6ef940d736fab8a1de558b75f21cb4fc
Arata-Kurokawa <arata.kurokawa@nextbeat.net>
2021-06-18 16:20:46 +0900

    TodoServiceを追加

---
+++ src/app/services/todo-service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Todo } from '@app/models/todo'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+  
+  getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>("/todoList")
+  }
+}

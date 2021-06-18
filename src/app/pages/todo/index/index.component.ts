656e2fc11e2e941634e6953442a968fd93155cb1
Arata-Kurokawa <arata.kurokawa@nextbeat.net>
2021-06-18 19:02:34 +0900

    Todo一覧をApiから取得して表示

--- src/app/pages/todo/index/index.component.ts
+++ src/app/pages/todo/index/index.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
 import { Todo } from '@app/models/todo'
+import { TodoService } from '@app/services/todo-service'
 
 @Component({
   selector: 'app-todo',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
-export class TodoIndexComponent {
-  title = "Todo一覧"
+export class TodoIndexComponent implements OnInit {
+  // todoList: Todo[] = [
+  //   { id: 1, title: "インフラ", body: "ESCを用いた開発環境の構築", state: Todo.Status.WAITING },
+  //   { id: 2, title: "バックエンド", body: "APIの実装", state: Todo.Status.IN_PROGRESS },
+  //   { id: 3, title: "フロント", body: "Angularの実装", state: Todo.Status.COMPLETED }
+  // ]
 
-  todoList: Todo[] = [
-    { id: 1, title: "インフラ", body: "ESCを用いた開発環境の構築", state: Todo.Status.WAITING },
-    { id: 2, title: "バックエンド", body: "APIの実装", state: Todo.Status.IN_PROGRESS },
-    { id: 3, title: "フロント", body: "Angularの実装", state: Todo.Status.COMPLETED }
-  ]
+  todoList: Todo[] = []
+
+  constructor(
+    private todoService: TodoService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.todoService.getAll().subscribe(data => {
+      this.todoList = data
+    })
+  }
 }

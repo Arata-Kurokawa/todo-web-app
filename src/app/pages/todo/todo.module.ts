409731be0e15311baeb3d0800b3ef561ab62cd48
Arata-Kurokawa <arata.kurokawa@nextbeat.net>
2021-06-18 15:21:02 +0900

    todo.moduleの修正

--- src/app/pages/todo/todo.module.ts
+++ src/app/pages/todo/todo.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TodoRoutingModule } from './todo-routing.module';
+
+import { TodoIndexComponent } from './index/index.component'
 
 @NgModule({
   imports: [
+    CommonModule,
     TodoRoutingModule
-  ]
+  ],
+  declarations: [TodoIndexComponent]
 })
 export class TodoModule { }

5911fd13a01b2c9c05336c2369b03d628cc38ddb
Arata-Kurokawa <ak.tumitaha@gmail.com>
2021-06-19 12:29:52 +0900

    app.moduleの場所を移動

--- src/app/pages/app/app-routing.module.ts
+++ src/app/pages/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: "todo",
-    loadChildren: () => import('./pages/todo/todo.module').then(m => m.TodoModule)
+    loadChildren: () => import('../todo/todo.module').then(m => m.TodoModule)
   }
 ];
 
